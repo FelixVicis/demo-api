1731f71f47107c18bc16cf834ee9966300963e60
Allen Mills <Mills.Allen.James@gmail.com>
2018-04-09 18:03:05 -0700

    build empty server

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Nothing to report');
+const express = require('express');
+
+const configuration = {
+	port:8080,
+	middleware:[],
+	routes:[
+	],
+};
+
+serve(configuration);
+
+
+function serve(configuration, done = () => null) {
+	const app = express();
+
+	app.set('port', configuration.port);
+
+
+	app.use([
+		...configuration.middleware,
+	]);
+
+	app.listen(app.get('port'), () => {
+		console.log(`Client started:  http://localhost:${app.get('port')}`); // eslint-disable-line no-console
+
+		done();
+	});
+
+	return app;
+};
+
